7d4774f48bc89af392e2b646de33ddb5da5ea686
Izhak <izhaklatovski@gmail.com>
2019-08-07 21:55:16 +0300

    Update Random Lottery.c

--- Exercise 3/Random Lottery.c
+++ Exercise 3/Random Lottery.c
@@ -4,14 +4,48 @@
  
 int main() {
 
- 
-	int random_numbers[6], i=0;
-	srand(time(NULL));
-    	for (i=0; i<6; i++) {
-		random_numbers[i]=(rand()%49)+1;
-		printf("%d\n",random_numbers[i]);
+	int random_numbers[6], i=0, r=0, c=0;
+	char board[5][10];
+	for(r=0 ; r<5 ; r++)        /* initialize board with '-' */
+    {
+	    for(c=0 ; c<10 ; c++)
+	    {
+	         board[r][c]='-';
+	    }
 	}
-
- 
+	    srand(time(NULL));      /* generate random numbers */
+    	for (i=0; i<6; i++)
+    	{
+    		random_numbers[i]=(rand()%49)+1;
+    		c=random_numbers[i]%10;
+        	random_numbers[i]/=10;
+    	    r=random_numbers[i];
+    	    while(board[r][c-1]=='+'){
+    	        random_numbers[i]=(rand()%49)+1;
+    	        c=random_numbers[i]%10;
+            	random_numbers[i]/=10;
+    	        r=random_numbers[i];
+    	    }
+    	    board[r][c-1]='+';
+	    }
+	    
+	    /* PRINTING-----------------------------------------*/
+	    printf(" 12345678910\n");
+	    for(i=0 ; i<4 ; i++)
+	    {
+	        printf("%d",i);
+	        for(r=0 ; r<10 ; r++)
+	        {
+	            printf("%c",board[i][r]);
+	        }
+	        printf("\n");
+	    }
+	    printf("4");
+	    for(r=0 ; r<9 ; r++)
+	    {
+	         printf("%c",board[4][r]);
+	    }
+	    printf("\n");
+	
 	return 0;
 }
