2eecbd7d7e8616ac4d17dffae059e0591f7c5f81
Izhak <izhaklatovski@gmail.com>
2019-08-12 11:21:10 +0300

    Add files via upload

---
+++ da.h
@@ -0,0 +1,17 @@
+#ifndef DA
+#define DA
+
+typedef struct
+{
+    int capacity;
+    int index;
+    int* arr; 
+} Array_t;
+
+Array_t* createArray(int capacity);
+int insert(Array_t* array, int number); 
+void print(Array_t* array);
+void destroy(Array_t* array);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "da.h"
+
+
+
+int main()
+{
+    int capacity=0, number=0, result=0;
+    Array_t* array;
+    printf("Insert capacity\n");
+    scanf("%d",&capacity);
+    array=createArray(capacity);
+    if(array==NULL)
+    {
+        return 0;
+    }
+    while(number!=(-1))
+    {
+        printf("Insert number\n");
+        scanf("%d",&number);
+        result=insert(array,number);
+    }
+    print(array);
+    destroy(array);
+    
+    return 0;
+}
