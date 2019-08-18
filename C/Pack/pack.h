6730a73459990ecfb1aa4f64eb72bd86cf97c20d
Izhak <izhaklatovski@gmail.com>
2019-08-18 14:04:21 +0300

    Update pack.h

--- C/Pack/pack.h
+++ C/Pack/pack.h
@@ -1,7 +1,19 @@
 #ifndef PACK_H
 #define PACK_H
 
+typedef struct 
+{
+	unsigned int left : 4;
+	unsigned int right: 4;
+} packStruct;
+
+typedef union
+{
+	packStruct 	lr;
+	char 		c;
+} un;
+
 int packString(char* string);
-/*int packStringUnion(char* string);*/
+int packStringUnion(char* string);
 
 #endif
