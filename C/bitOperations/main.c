debf1c6f490d09a6f2f4f787a5c30ad28b884e3a
Izhak <izhaklatovski@gmail.com>
2019-08-19 09:02:53 +0300

    Update main.c

--- C/bitOperations/main.c
+++ C/bitOperations/main.c
@@ -6,24 +6,5 @@ int main()
 {
 	unsigned char x=86;
 	invert(x);
-
-	setbitsTwo(0,3,5,1);
-
 	return 0;
-}
-
-int setbits(unsigned char x, unsigned int p, unsigned int n, unsigned char y)
-{
-	unsigned char temp=x;
-
-	temp=temp>>p;
-	temp=temp<<p;
-	x=x<<(8-p+n);
-	x=x>>(8-p+n);
-	y=y<<(8-n);
-	y=y>>(8-p);
-	x=(x|temp);
-	x=x|y;
-
-	return x;
 }
