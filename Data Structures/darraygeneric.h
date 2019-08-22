b97c65c811e90e9a0b7b4b9587c8c44f151cf97e
Izhak <izhaklatovski@gmail.com>
2019-08-22 09:48:32 +0300

    Update darraygeneric.h

--- Data Structures/darraygeneric.h
+++ Data Structures/darraygeneric.h
@@ -27,6 +27,4 @@ AdtStatus   darraySet(darray *dArr, size_t _index, void  *_item);
 AdtStatus   darrayItemsNum(darray *dArr, int*  _numOfItems);​
 /* in place (ascending) sorting an array of size arraySize */
 AdtStatus darraySort(darray *dArr, elementCompare compareFunc); /* correct this !!! */
-/*MY FUNCTION FOR TESTING*/
-AdtStatus darrayPrint(darray *dArr, elementPrint printFunc);
 #endif
