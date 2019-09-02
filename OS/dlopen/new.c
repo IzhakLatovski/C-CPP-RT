fc188ea3ce21bfb29c7f31e93c1ae4d7d83a2650
Izhak <izhaklatovski@gmail.com>
2019-09-02 20:04:39 +0300

    Add files via upload

--- OS/dlopen/new.c
+++ OS/dlopen/new.c
@@ -5,10 +5,18 @@
 
 int main()
 {
+	void (*runLib)(void);
+    char function[64];
 	void* handle=dlopen("libN.so",RTLD_LAZY);
+
+	printf("Enter library:\n");
+    scanf("%s",function);
+	*(void **)(&runLib) = dlsym(handle, func);
+	runLib();
 	lib1func();
 	lib2func();
 	lib3func();
+	dlclose(handle);
 
 	return 0;
 }
