bbe72a4f85f6f6294d7df528d00e9231650c6d38
Izhak <izhaklatovski@gmail.com>
2019-09-08 11:57:59 +0300

    Add files via upload

--- OS/threadIncrement.c
+++ OS/threadIncrement.c
@@ -11,11 +11,10 @@ int main()
 {
 	pthread_t tid;
 	int i=0;
-	int array[100];
 
 	for(i=0 ; i<100 ; i++)
 	{
-		array[i]=pthread_create(&tid,NULL,increment,NULL);
+		pthread_create(&tid,NULL,increment,NULL);
 		pthread_join(tid,NULL);
 	}
 	printf("---%d---\n",count);
