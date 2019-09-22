d3139311c2d09492b30de4e40d98c6b6a3022935
Izhak <izhaklatovski@gmail.com>
2019-09-22 17:30:14 +0300

    Update string_t.cpp

--- CPP/string_t/string_t.cpp
+++ CPP/string_t/string_t.cpp
@@ -1,23 +1,27 @@
 #include "string_t.h"
+#include <stdio.h>
 
 string_t::string_t()
 {
 	name="0";
+        len=0;
 }
 
 string_t::string_t(const char* nm)
 {
-	name=nm;
+	name=new char[strlen(nm)+1];
+        strcpy(name,nm);
+        len=strlen(nm);
 }
 
 string_t::~string_t()
 {
-
+        delete[] name;
 }
 
 string_t::string_t(const string_t& st)
 {
-	name=st.name;
+	name=new char[strlen()+1] ddddddd
 }
 
 void string_t::operator=(const string_t& st)
@@ -31,7 +35,7 @@ void string_t::operator=(const string_t& st)
 
 int string_t::length() const
 {
-	return strlen(name);
+	return len;
 }
 
 void string_t::setString(const char* nm)
