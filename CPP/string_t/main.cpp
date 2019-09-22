4958ae4d75555f435fe5ea47763bdeb8efe89bf6
Izhak <izhaklatovski@gmail.com>
2019-09-22 22:56:34 +0300

    Update main.cpp

--- CPP/string_t/main.cpp
+++ CPP/string_t/main.cpp
@@ -1,4 +1,4 @@
-#include "string_t.h"
+#include "1.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,30 +6,43 @@
 
 int main()
 {
-	string_t temp;
+	int chosen=1;
+	char* string;
+	while(chosen>0 && chosen<10)
+	{
+		printf("Please choose an option:\n");
+		printf("1. Create empty string:\n");
+		printf("2. Create string:\n");
+		printf("3. Copy string:\n");
+		printf("4. Use assignment:\n");
+		printf("5. Set string:\n");
+		printf("6. Get string:\n");
+		printf("7. Get length:\n");
+		printf("8. Compare 2 strings:\n");
+		printf("9. Print string:\n");
+		printf("Other - EXIT\n");
+
+		scanf("%d",&chosen);
+		switch(chosen)
+		{
+			case 5:
+				printf("Enter the string\n");
+				scanf("%s",string);
+				string_t case2=string_t(string);
+		}
+	}
+/*
 	string_t temp1("izhak");
 	temp1.print();
 	temp1.setString("Yossi");
 	temp1.print();
 	const char* gotName=temp1.getString();
-	printf("%s",gotName);
+	printf("%s\n",gotName);
+	temp1.setString("Rita");
 	string_t temp2(temp1);
 	temp2.print();
-	temp1.setString("Rita");
-	string_t temp3=temp1;
-	temp3.print();
-	int len=temp3.length();
-	printf("%d\n",len);
-
-	temp1.setString("Qri");
-	temp2.setString("Ori");
-
-	int compared=temp1.compare(temp2);
-	printf("%d\n",compared);
-
-	temp.setString("AAAAAA");
-	temp.print();
-
-
+	int ln=temp1.length();
+	printf("%d\n",ln);
+*/
 	return 0;
 }
