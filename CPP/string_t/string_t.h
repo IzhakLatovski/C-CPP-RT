3b615cb623f8d25b80df91aeccd9a179964e214e
Izhak <izhaklatovski@gmail.com>
2019-09-23 12:49:15 +0300

    Update string_t.h

--- CPP/string_t/string_t.h
+++ CPP/string_t/string_t.h
@@ -6,17 +6,25 @@
 class string_t
 {
 	public:
-		string_t();
-		string_t(const char* nm);
-		~string_t();
-		string_t(const string_t& st);
+		~string_t();							/*Destructor*/
+		string_t();								/*Default constructor*/
+		string_t(const char* nm);				/*Type constructor*/
+		string_t(const string_t& st);			/*Copy constructor*/
 		void operator=(const string_t &st);
+		int operator<(const string_t &st);
+		int operator>(const string_t &st);
+		int operator<=(const string_t &st);
+		int operator>=(const string_t &st);
+		int operator==(const string_t &st);
+		int operator!=(const string_t &st);
 
 		int length() const;
 		void setString(const char* nm);
 		const char* getString() const;
 		int compare(const string_t st2) const;
 		void print() const;
+		void toUpper();
+		void toLower();
 
 	private:
 		char* name;
