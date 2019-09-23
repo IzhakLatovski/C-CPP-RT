e0413b382b3d54fc975976d93eb35f08dde14f85
Izhak <izhaklatovski@gmail.com>
2019-09-23 12:48:59 +0300

    Update string_t.cpp

--- CPP/string_t/string_t.cpp
+++ CPP/string_t/string_t.cpp
@@ -1,9 +1,10 @@
 #include "string_t.h"
 #include <stdio.h>
+#include <ctype.h>
 
 string_t::string_t()
 {
-	name=new char[16];
+	name=new char[32];
 	name[0]='\0';
     len=0;
 }
@@ -55,10 +56,106 @@ const char* string_t::getString() const
 
 int string_t::compare(string_t st2) const
 {
-	return (strcmp(name,st2.name));
+	int result=(strcmp(name,st2.name));
+	if(result==0)
+	{
+		return 0;
+	}
+	else if(result>0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 2;
+	}
 }
 
 void string_t::print() const
 {
 	printf("%s\n",name);
+}
+/*********************************************************************************/
+void string_t::toLower()
+{
+	int i=0;
+	char c;
+	while(name[i])
+	{
+		c=name[i];
+		name[i]=tolower(c);
+		i++;
+	}
+}
+
+void string_t::toUpper()
+{
+	int i=0;
+	char c;
+	while(name[i])
+	{
+		c=name[i];
+		name[i]=toupper(c);
+		i++;
+	}
+}
+
+int string_t::operator<(const string_t& st)
+{
+	if(strcmp(name,st.name)<0)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+int string_t::operator>(const string_t& st)
+{
+	if(strcmp(name,st.name)>0)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int string_t::operator<=(const string_t& st)
+{
+	if(strcmp(name,st.name)<0 || strcmp(name,st.name)==0)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int string_t::operator>=(const string_t& st)
+{
+	if(strcmp(name,st.name)>0 || strcmp(name,st.name)==0)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int string_t::operator==(const string_t& st)
+{
+	if(strcmp(name,st.name)==0)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int string_t::operator!=(const string_t& st)
+{
+	if(strcmp(name,st.name)==0)
+	{
+		return 0;
+	}
+	
+	return 1;
 }
