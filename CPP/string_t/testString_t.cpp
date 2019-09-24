4fea9eede2dc6cce1eedeba4f449215dba75993b
Izhak <izhaklatovski@gmail.com>
2019-09-24 22:34:59 +0300

    Update and rename main.cpp to testString_t.cpp

--- CPP/string_t/testString_t.cpp
+++ CPP/string_t/testString_t.cpp
@@ -1,3 +1,10 @@
+/********************************************************/
+/*		                                                */
+/*      NO TIME TO MAKE THE TESTING MODULE PROPERLY     */
+/* The second part(bottom note), tests all the features */  										 */
+/*  										            */
+/********************************************************/
+
 #include "string_t.h"
 
 #include <stdio.h>
@@ -8,31 +15,73 @@
 using namespace std;
 
 int main()
-{/*
-	int chosen=1, index=0;
-	char* temp;
+{
+	int chosen=1, objectNumber=0;
+	char testString[64];
 	string_t string1, string2;
+	printf("You have 2 empty objects, and now create 3rd with your text:\n");
+	printf("Enter your string:\n");
+	scanf("%s",testString);
+	string_t string3(testString);
 	while(chosen>0 && chosen<10)
 	{
 		printf("Please choose an option:\n");
-		printf("1. Create empty string:\n");
-		printf("2. Create string:\n");
+		printf("1. Print string:\n");
+		printf("2. Change string:\n");
+
+		/*
 		printf("3. Copy string:\n");
 		printf("4. Use assignment:\n");
 		printf("5. Set string:\n");
 		printf("6. Get string:\n");
 		printf("7. Get length:\n");
 		printf("8. Compare 2 strings:\n");
-		printf("9. Print string:\n");
+		printf("9. Print string:\n");*/
 		printf("Other - EXIT\n");
 		scanf("%d",&chosen);
 		switch(chosen)
 		{
 			case 1:
 			{
-				string1();
+				printf("Enter the object number (1/2/3)\n");
+				scanf("%d",&objectNumber);
+				if(objectNumber==1)
+				{
+					string1.print();
+				}
+				else if(objectNumber==2)
+				{
+					string2.print();
+				}
+				else if(objectNumber==3)
+				{
+					string3.print();
+				}
 				break;
-			}
+			}/*
+			case 2:
+			{
+				printf("Enter the object number (1/2/3)\n");
+				scanf("%d",&objectNumber);
+				printf("Enter the string to put\n");
+				scanf("%s",&testString);
+				if(objectNumber==1)
+				{
+					string1.setString(testString);
+				}
+				else if(objectNumber==2)
+				{
+					string2.setString(testString);
+				}
+				else if(objectNumber==3)
+				{
+					string3.setString(testString);
+				}
+				break;
+			}*/
+		}
+	}
+			/*
 			case 2:
 			{
 				printf("Enter the string to put in the new object:\n");
@@ -44,22 +93,23 @@ int main()
 			{
 				string2.print();
 				break;
-			}/*
+			}
 			case 5:
 				printf("Enter the string\n");
 				scanf("%s",temp);
-				string_t case2=string_t(temp);
-		}*/
-	
-
+				string_t case2=string_t(temp);*/
+	/*
 	string_t temp1("izhak");
 	temp1.print();
 	temp1.setString("Yossi");
 	temp1.print();
+	printf("NUMBER OF OBJECTS : %lu\n",temp1.getNumberOfObjects());
+
 	const char* gotName=temp1.getString();
 	printf("%s\n",gotName);
 	temp1.setString("RITA");
 	string_t temp2(temp1);
+	printf("NUMBER OF OBJECTS : %lu\n",temp1.getNumberOfObjects());
 	temp2.print();
 	int ln=temp1.length();
 	printf("%d\n",ln);
@@ -72,7 +122,7 @@ int main()
 	temp2.toLower();
 	temp1.print();
 	temp2.print();
-	temp1.setString("Izhak");
+	temp1.setString("Izhakizhaka");
 	temp2.setString("izhAk");
 	printf("----%d----\n",(temp1>temp2));
 	printf("----%d----\n",(temp1<temp2));
@@ -80,7 +130,12 @@ int main()
 	printf("----%d----\n",(temp1<=temp2));
 	printf("----%d----\n",(temp1==temp2));
 	printf("----%d----\n",(temp1!=temp2));
-	printf("%lu\n",temp1.firstOccurrence('k'));/*
+	printf("%lu\n",temp1.firstOccurrence('a'));
+	printf("%lu\n",temp1.lastOccurrence('A'));
+	printf("NUMBER OF OBJECTS : %lu\n",temp2.getNumberOfObjects());
+	temp1.print();/*
+	string_t temp3=temp1(8,4);
+	temp3.print();/*
 	temp1+=temp2;
 	temp1.print();
 	temp1.setString("rita");
