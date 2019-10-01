22697d7e9687bffac1da69d0408c01473440b4a7
Izhak <izhaklatovski@gmail.com>
2019-10-01 23:32:28 +0300

    Update memPage.h

--- CPP/memManager_t/memPage.h
+++ CPP/memManager_t/memPage.h
@@ -1,4 +1,21 @@
-#include "memoryManager.h"
+#include "memManager.h"
 
-# include <iostream>
+#include <iostream>
 #include <string.h>
+
+class memPage_t: public memManager_t{
+   public:
+      ~memPage_t();
+      memPage_t();
+      memPage_t(size_t capacity)
+      {
+           m_capacity=capacity;
+      }
+      size_t getCapacity() const
+      {
+            return m_capacity;
+      }
+      bool isFull() const;
+     
+  private:
+     size_t m_capacity;
