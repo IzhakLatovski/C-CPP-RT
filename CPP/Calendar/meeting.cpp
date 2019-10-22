67c553d20a73fceb3b895b1813dee2da6f998077
Izhak <izhaklatovski@gmail.com>
2019-10-22 09:02:09 +0300

    Update meeting.cpp

--- CPP/Calendar/meeting.cpp
+++ CPP/Calendar/meeting.cpp
@@ -9,17 +9,20 @@ using namespace std;
 
 meeting_t::meeting_t(const string& subject, float begin, float end):m_subject(subject)
 {
-	if(begin<0 || end>24)
+	try
 	{
-		throw(1);
-	}
-	else if(begin>=end)
-	{
-		throw(2);
-	}
-	else
-	{
-		m_begin=begin;
-		m_end=end;
+		if(begin<0 || end>24)
+		{
+			throw(1);
+		}
+		else if(begin>=end)
+		{
+			throw(2);
+		}
+		else
+		{
+			m_begin=begin;
+			m_end=end;
+		}
 	}
 }
